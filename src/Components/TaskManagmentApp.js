6f0e7547f19cc03f9a19455acde6412ae2e738a6
Nikhil26062000 <nikhilsbn06@gmail.com>
2023-11-28 18:35:24 +0530

    initial commit

---
+++ src/Components/TaskManagmentApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css'
+
+const TaskManagmentApp = () =>{
+    return(
+        <div>
+            <h1 className="text-green-700">Hello World</h1>
+        </div>
+    )
+}
+
+export default TaskManagmentApp
